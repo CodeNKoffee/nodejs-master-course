d947c444fe43202c4e878c909d910b4920ebc01d
Hatem Soliman <hatemthedev@gmail.com>
2023-08-31 14:54:52 +0300

    Section 2 + Scaffoldiing, Starting a Server, Parsing Request Paths, Parsing HTTP Methods, Parsing Query Strings

---
+++ Section 3/app/index.js
@@ -0,0 +1,34 @@
+/**
+ * Primary file for API
+ */
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+
+// The server should respond to all requests with a string 
+var server = http.createServer(function(req, res) {
+
+  // Get the URL and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path from the URL
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
+
+  // Send the response
+  res.end('Hello, World!\n');
+
+  // Log the request path
+  console.log('Request received on path: '+ trimmedPath +'with method: '+ method);
+  
+});
+
+// Start the server and have it listening on port 3000
+server.listen(3000, function() {
+  console.log("The server is listening on port 3000 now...");
+});
+
