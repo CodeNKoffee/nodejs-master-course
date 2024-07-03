dca2d93ba356270a14febfbae3a2feaded18d7c4
Hatem Soliman <hatemthedev@gmail.com>
2024-07-03 16:36:28 +0300

    DELETE token request completed

--- Section 3/app/lib/handlers.js
+++ Section 3/app/lib/handlers.js
@@ -300,8 +300,32 @@ handlers._tokens.put = function(data, callback) {
 };
 
 // Tokens - delete
+// Required field: id
+// Optional data: none
 handlers._tokens.delete = function(data, callback) {
+  // Check that the id number is valid
+  var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length == 20 ? data.queryStringObject.id.trim() : false;
 
+  if (id) {
+    // Lookup the token
+    _data.read('tokens', id, function(err, data) {
+      if (!err && data) {
+        _data.delete('tokens', id, function(err) {
+          if (!err) {
+            callback(200);
+          } else {
+            callback(500, {'Error': 'Could not delete the specified user'});
+          }
+        });
+        callback(200);
+      } else {
+        console.log("error: ", err);
+        callback(400, {'Error': 'Could not find the specified user'});
+      }
+    })
+  } else {
+    callback(400, {'Error': 'Missing required field'});
+  }
 };
 
 // Ping handler
